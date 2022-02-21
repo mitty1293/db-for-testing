ab0b11ddcf3dadfdd37f76dbb5d1d68086e5a000
mitty1293 <k.fmichi@gmail.com>
2022-02-21 21:00:00 +0900

    テーブル初期化SQLの配置

---
+++ mysql/initdb.d/create_tbl.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `mysql_test_db`.`mysql_test_tbl`(
+    `id`        INT unsigned NOT NULL AUTO_INCREMENT,
+    `column1`   VARCHAR(10),
+    `column2`   VARCHAR(10),
+    PRIMARY KEY (`id`)
+);
